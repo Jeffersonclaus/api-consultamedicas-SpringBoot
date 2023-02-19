85696e7e54ff4c690fb42be73a8844582138372c
claus <claus@JC1>
2023-02-18 23:22:26 -0300

    Concluído CRUD

--- api/src/main/java/med/voll/api/endereco/Endereco.java
+++ api/src/main/java/med/voll/api/endereco/Endereco.java
@@ -12,8 +12,7 @@ import med.voll.api.medico.DadosEndereco;
 @NoArgsConstructor
 @AllArgsConstructor
 public class Endereco {
-	
-	
+
 	private String logradouro;
 	private String bairro;
 	private String cep;
@@ -21,20 +20,43 @@ public class Endereco {
 	private String complemento;
 	private String cidade;
 	private String uf;
-	
-	
-	
-public Endereco(DadosEndereco dados) {
-	this.logradouro= dados.logradouro();
-	this.bairro= dados.bairro();
-	this.cep= dados.cep();
-	this.uf= dados.uf();
-	this.numero= dados.numero();
-	this.cidade= dados.cidade();
-	this.complemento= dados.complemento();
-	
-	
-	
+
+	public Endereco(DadosEndereco dados) {
+		this.logradouro = dados.logradouro();
+		this.bairro = dados.bairro();
+		this.cep = dados.cep();
+		this.uf = dados.uf();
+		this.numero = dados.numero();
+		this.cidade = dados.cidade();
+		this.complemento = dados.complemento();
+
 	}
-	
+
+	public void atualizarInformacoes(DadosEndereco dados) {
+		if (dados.logradouro() != null) {
+			this.logradouro = dados.logradouro();
+		}
+
+		if (dados.bairro() != null) {
+			this.bairro = dados.bairro();
+		}
+		if (dados.cep() != null) {
+			this.cep = dados.cep();
+		}
+		if (dados.uf() != null) {
+			this.uf = dados.uf();
+		}
+
+		if (dados.numero() != null) {
+			this.numero = dados.numero();
+		}
+		if (dados.cidade() != null) {
+			this.cidade = dados.cidade();
+		}
+		if (dados.complemento() != null) {
+			this.complemento = dados.complemento();
+		}
+
+	}
+
 }
--- api/src/main/java/med/voll/api/medico/Medico.java
+++ api/src/main/java/med/voll/api/medico/Medico.java
@@ -15,10 +15,8 @@ import lombok.NoArgsConstructor;
 import lombok.Setter;
 import med.voll.api.endereco.Endereco;
 
-
-
-@Table(name= "medicos")
-@Entity(name= "Medicos")
+@Table(name = "medicos")
+@Entity(name = "Medicos")
 
 @Getter
 @Setter
@@ -26,33 +24,57 @@ import med.voll.api.endereco.Endereco;
 @AllArgsConstructor
 @EqualsAndHashCode(of = "id")
 public class Medico {
-	@Id @GeneratedValue(strategy = GenerationType.IDENTITY)
-	private String id;
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
 	private String nome;
 	private String email;
-	private String telefone;	
+	private String telefone;
 	private String crm;
 	@Enumerated(EnumType.STRING)
-	private Especialidade especialidade; 
+	private Especialidade especialidade;
 	@Embedded
 	private Endereco endereco;
-	
-	
-	
+
+	private Boolean ativo;
+
 	public Medico(DadosCadastroMedico dados) {
-		
+
+		this.ativo = true;
+		this.nome = dados.nome();
+		this.email = dados.email();
+		this.telefone = dados.telefone();
+		this.crm = dados.crm();
+		this.especialidade = dados.especialista();
+		this.endereco = new Endereco(dados.endereco());
+
+	}
+
+	public void excluir() {
+		this.ativo = false;
+
+	}
+
+	public void atualizarInformacoes(DadosAtualizacaoMedico dados) {
+
+		if(dados.nome() !=null ) {
 		this.nome=dados.nome();
-		this.email=dados.email();
+
+		}
+		
+
+		if(dados.telefone() !=null ) {
 		this.telefone=dados.telefone();
-		this.crm=dados.crm();
-		this.especialidade=dados.especialista();
-		this.endereco= new Endereco(dados.endereco());
+
+		}
+		
+
+		if(dados.endereco()!=null ) {
+		this.endereco.atualizarInformacoes(dados.endereco());
+
+		}
+		
 		
 	}
 
-
-	
-	
-	
-	
 }
